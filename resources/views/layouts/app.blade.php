aadf21629708289a9148535b4946b9f8437d2c43
Richard Pilbery <tricky999@Richards-iMac.local>
2020-03-23 21:42:08 +0000

    Missed a BATS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>BATS</title>
+    <title>BATT</title>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
